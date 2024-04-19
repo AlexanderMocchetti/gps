47f9a339e60ebbf6bf69416946a1e29863b4d901
alexandermocchetti <mocchettialexander@gmail.com>
2024-04-19 10:18:06 +0200

    commit #1

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+require_once("conn.php");
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Random Point</title>
+    <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.css" />
+    <style>
+        html,
+        body,
+        #map {
+            width: 100%;
+            height: 100%;
+            margin: 0;
+            padding: 0;
+        }
+    </style>
+</head>
+
+<body>
+    <div id="map"></div>
+    <script src="http://cdn.leafletjs.com/leaflet-0.7.3/leaflet.js"></script>
+    <script>
+        var mapOptions = {
+            center: [42.407, 13.096],
+            zoom: 5
+        }
+
+        var map = new L.map('map', mapOptions);
+
+        var layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+
+        map.addLayer(layer);
+
+        <?php
+
+        $query = "SELECT lat, lng FROM waypoints";
+        $result = $mysqli->query($query);
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                echo "L.marker([" . $row['lat'] . ", " . $row['lng'] . "]).addTo(map);\n";
+            }
+        } else {
+            echo "0 risultati";
+        }
+        ?>
+    </script>
+</body>
+
+</html>
